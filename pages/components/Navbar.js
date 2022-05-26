8db51acaad80eb97c81c36ab4c6b9c75f3560c63
andres <echeverry11298@gmail.com>
2022-05-26 16:52:27 -0500

    finalizamos la primera pagina

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,12 +1,18 @@
 import styles from "../../styles/Navbar.module.css";
 
 function Navbar() {
-    return (
+  return (
     <nav className={styles.menu}>
-      <a href="#home">Inicio</a>
-      <a href="#about">acerca</a>
+      <div className={styles.links}>
+        <a href="#home">Inicio</a>
+        <a href="#about">acerca</a>
+        <a href="#projects">proyectos</a>
+      </div>
+      <div className={styles.containerBtn}>
+        <button>Engl</button>
+      </div>
     </nav>
-    )
+  );
 }
 
 export default Navbar;
