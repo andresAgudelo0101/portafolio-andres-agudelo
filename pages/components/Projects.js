6f388bce2281632397bbd66759a81875f13241b7
andres <echeverry11298@gmail.com>
2022-06-06 20:45:08 -0500

    ya encontramos la via

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -33,24 +33,23 @@ function Projects() {
 
   return (
     <>
-      <div>
+      <div id="projects">
         <div className={styles.container}>
-        
-          <h1 className={styles.titulo}>Proyectos Realizados</h1>
-          <div className={styles.x}>
-          <div className={styles.containerLista}>
-            {proyectos.map((proyecto) => (
-              <div className={styles.card} to={proyecto.path} key={proyecto.id}>
-                <p className={styles.tituloCard}>{proyecto.name}</p>
-                <img
-                  src={proyecto.img}
-                  className={styles.img1}
-                  alt={proyecto.alt}
-                />
-              </div>
-            ))}
+          <div class={styles.slider}>
+            <a href="#slide-1">1</a>
+            <a href="#slide-2">2</a>
+            <a href="#slide-3">3</a>
+            <a href="#slide-4">4</a>
+            <a href="#slide-5">5</a>
+
+            <div class={styles.slides}>
+              <div id="slide-1">1</div>
+              <div id="slide-2">2</div>
+              <div id="slide-3">3</div>
+              <div id="slide-4">4</div>
+              <div id="slide-5">5</div>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </>
